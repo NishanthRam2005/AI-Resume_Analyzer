4bcf92d3e37da74690e4243e9df295b66b23791c
Nishanth Ram K <kkalimuthu818@gmail.com>
2025-06-19 13:14:00 +0530

    Updates

---
+++ _next/static/chunks/app/layout-e4d6bb1a80ef1f82.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[185],{5522:function(n,e,u){Promise.resolve().then(u.t.bind(u,2445,23))},2445:function(){}},function(n){n.O(0,[971,938,744],function(){return n(n.s=5522)}),_N_E=n.O()}]);
